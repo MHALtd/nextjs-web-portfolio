86364b932f166860b1a6b32b76f96e97f00653e1
MHALtd <hasbi1117@gmail.com>
2024-09-18 19:39:05 +0700

    changes footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer border border-top-tartiary border-l-transparent border-r-transparent text-tartiary">
       <div className="container p-5 flex justify-between">
-        <span>Muhammad Hasbi Abdillah</span>
+        <span>MHALtd</span>
         <p className=" text-slate-500">All rights reserved.</p>
       </div>
     </footer>
